1f312a346f87e8849e2c6d2fbce9fd4bcd4b2e10
[dyad] <chebad619@gmail.com>
2025-10-27 18:52:05 +0300

    Reverted all changes back to version 0781104702538ee926fa017ed742ad51a500804c

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashboardPage from "./pages/DashboardPage"; // Corrected import path after rename
-import PatientManagementPage from "./pages/PatientManagementPage";
-import ExaminationPage from "./pages/ExaminationPage";
-import InvestigationsPage from "./pages/InvestigationsPage";
-import PrescriptionServicesPage from "./pages/PrescriptionServicesPage";
-import ReferralFollowupPage from "./pages/ReferralFollowupPage";
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,12 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/patient-management" element={<PatientManagementPage />} />
-          <Route path="/examination" element={<ExaminationPage />} />
-          <Route path="/investigations" element={<InvestigationsPage />} />
-          <Route path="/prescription-services" element={<PrescriptionServicesPage />} />
-          <Route path="/referral-followup" element={<ReferralFollowupPage />} />
+          <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
