2a031a78859c5a5379026eece23bf9ef2a3bec72
[dyad] <chebad619@gmail.com>
2025-10-27 18:24:57 +0300

    [dyad] Added Prescription & Services page and form - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import PatientManagementPage from "./pages/PatientManagementPage";
-import ExaminationPage from "./pages/ExaminationPage"; // New import
+import ExaminationPage from "./pages/ExaminationPage";
 import InvestigationsPage from "./pages/InvestigationsPage";
+import PrescriptionServicesPage from "./pages/PrescriptionServicesPage"; // New import
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,8 +21,9 @@ const App = () => (
         <Routes>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/patient-management" element={<PatientManagementPage />} />
-          <Route path="/examination" element={<ExaminationPage />} /> {/* New route */}
+          <Route path="/examination" element={<ExaminationPage />} />
           <Route path="/investigations" element={<InvestigationsPage />} />
+          <Route path="/prescription-services" element={<PrescriptionServicesPage />} /> {/* New route */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
