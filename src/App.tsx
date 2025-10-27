a6314c273909cb509ca3584b4a8c494f822373d3
[dyad] <chebad619@gmail.com>
2025-10-27 18:18:57 +0300

    Reverted all changes back to version 979b1fea53949b1b40d637cde852da525a66bbc8

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
-import PatientManagementPage from "./pages/PatientManagementPage";
-import InvestigationsPage from "./pages/InvestigationsPage"; // New import
+import PatientManagementPage from "./pages/PatientManagementPage"; // New import
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,8 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<DashboardPage />} />
-          <Route path="/patient-management" element={<PatientManagementPage />} />
-          <Route path="/investigations" element={<InvestigationsPage />} /> {/* New route */}
+          <Route path="/patient-management" element={<PatientManagementPage />} /> {/* New route */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
