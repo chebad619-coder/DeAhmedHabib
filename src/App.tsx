3efd48c0e65add9a17aeafc6481e6682073c2a1e
[dyad] <chebad619@gmail.com>
2025-10-27 18:17:50 +0300

    [dyad] Adding investigations module - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
-import PatientManagementPage from "./pages/PatientManagementPage"; // New import
+import PatientManagementPage from "./pages/PatientManagementPage";
+import InvestigationsPage from "./pages/InvestigationsPage"; // New import
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -17,7 +18,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<DashboardPage />} />
-          <Route path="/patient-management" element={<PatientManagementPage />} /> {/* New route */}
+          <Route path="/patient-management" element={<PatientManagementPage />} />
+          <Route path="/investigations" element={<InvestigationsPage />} /> {/* New route */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
